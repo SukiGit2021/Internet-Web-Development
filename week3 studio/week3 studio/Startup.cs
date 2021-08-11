a748e28401ac090f522860f26e10d5d8bc84d50b
SukiGit2021 <pliu0030@student.monash.edu>
2021-08-11 18:42:18 +1000

    week 3 studio tasks first commit
    try first time git doc

---
+++ week3 studio/week3 studio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(week3_studio.Startup))]
+namespace week3_studio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
