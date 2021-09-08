aee426e142563aea217ffdd6b9c8d70952fbb3d8
SukiGit2021 <pliu0030@student.monash.edu>
2021-09-08 20:39:15 +1000

    Add project files.
    Login & Registration authentication

---
+++ FIT5032_login_regist/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_login_regist.Startup))]
+namespace FIT5032_login_regist
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
