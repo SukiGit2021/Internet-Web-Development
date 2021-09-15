b4281963ae4c7e6d5356c6742053a32c8f180e4c
SukiGit2021 <pliu0030@student.monash.edu>
2021-09-15 19:00:42 +1000

    Add project files.

---
+++ FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("liupeiyupetty@163.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "peiyu228liu@gmail");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
