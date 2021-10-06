24012cdbd1cc4178fd0f3255bd11cf47b3aeabde
SukiGit2021 <pliu0030@student.monash.edu>
2021-10-06 17:51:19 +1100

    10/06

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿
+SP_RENAME 'AspNetUserRoles.Roles_Id', 'RoleId','Column';
+SP_RENAME 'AspNetUserRoles.Users_Id', 'UserId','Column';
+
+
+
+/*SP_RENAME 'TableName.ColumnName', 'NewColumnName', 'Column';*/
